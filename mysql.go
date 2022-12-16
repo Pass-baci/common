d2a052caeb8dc9ec2fbd0780fee8796fc52c5285
v_chgvchen <v_chgvchen@tencent.com>
2022-12-16 15:52:06 +0800

    添加获取mysql配置

---
+++ mysql.go
@@ -0,0 +1,20 @@
+package common
+
+import "github.com/asim/go-micro/v3/config"
+
+// MysqlConfig Mysql配置结构体
+type MysqlConfig struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Pwd      string `json:"pwd"`
+	Database string `json:"database"`
+	Port     string `json:"port"`
+}
+
+func GetMysqlFromConsul(config config.Config, path ...string) (*MysqlConfig, error) {
+	mysqlConfig := &MysqlConfig{}
+	if err := config.Get(path...).Scan(mysqlConfig); err != nil {
+		return nil, err
+	}
+	return mysqlConfig, nil
+}
