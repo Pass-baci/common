3415fbe7c3b90d19dee42b201b2316c9a7dd7cdf
v_chgvchen <v_chgvchen@tencent.com>
2022-12-20 10:57:25 +0800

    添加监控

---
+++ prometheus.go
@@ -0,0 +1,18 @@
+package common
+
+import (
+	"github.com/asim/go-micro/v3/util/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"strconv"
+)
+
+func PrometheusBoot(port int) {
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		if err := http.ListenAndServe("0.0.0.0"+strconv.Itoa(port), nil); err != nil {
+			log.Fatalf("启动Prometheus失败 err: %s", err.Error())
+		}
+		log.Info("启动Prometheus成功，端口为：" + strconv.Itoa(port))
+	}()
+}
